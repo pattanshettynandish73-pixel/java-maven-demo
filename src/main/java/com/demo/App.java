4a2fa193393faebb32d694f27d47549b6c2c6067
Nandishpattanshetty <nandishpv080@gmail.com>
2026-03-02 10:09:33 +0530

    Java code uploaded

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps!");
+    }
+}
